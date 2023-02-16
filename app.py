f994e3d52df07d7163ab379672c587e9b49fe800
mariusfali <marius.falise@gmail.com>
2023-02-16 13:58:02 +0100

    Laget lag side og legg til/fjern spiller

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from scraping import scrape_word
 from scraping2 import sjekk_alle_artikler
+import json
 
 app = Flask(__name__)
+
+
+fil = open("politikere.json", encoding="utf-8")
+politikere = json.load(fil)
+fil.close()
+
+#print(politikere["representanter_oversikt"]["representanter_liste"]["representant"][0]["fornavn"] + politikere["representanter_oversikt"]["representanter_liste"]["representant"][0]["etternavn"])
+alle_politikere = []
+for i in politikere["representanter_oversikt"]["representanter_liste"]["representant"]:
+    #print(i["fornavn"])
+    alle_politikere.append(f'{i["fornavn"]} {i["etternavn"]}')
+
+
+fantasy_lag = []
+
+#print(alle_politikere)
+
 
 @app.route("/")
 def index():
@@ -21,6 +39,23 @@ def results_page():
 
 @app.route("/lag")
 def lag_page():
-    return render_template("lag.html")
+    return render_template("lag.html", fantasy_lag=fantasy_lag, alle_politikere=alle_politikere)
+
+@app.route("/legg-til-politiker", methods=['POST','GET'])
+def legg_til_politiker_funksjon():
+    if request.method == 'POST':
+        politiker = request.form.get("politiker")
+        if len(fantasy_lag) < 8:
+            fantasy_lag.append(politiker)
+            return render_template("lag.html", fantasy_lag=fantasy_lag, alle_politikere=alle_politikere)
+        else:
+            return render_template("lag.html", fantasy_lag=fantasy_lag, alle_politikere=alle_politikere)
+        
+@app.route("/fjern-politiker/<int:id>", methods=['POST','GET'])
+def fjern_politiker_funksjon(id):
+    if request.method == 'POST':
+        fantasy_lag.pop(id-1)
+    return render_template("lag.html", fantasy_lag=fantasy_lag, alle_politikere=alle_politikere)
+        
 
 app.run(debug=True)
